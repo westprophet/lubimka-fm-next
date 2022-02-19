0d6675cd87de7df883be69a2fc6fc6d019420696
westprophet <PROPHET44556677889900>
2022-02-19 20:16:43 +0200

    dev

--- src/validations/isValidICompany.ts
+++ src/validations/isValidICompany.ts
@@ -2,7 +2,7 @@
 export default function isValidICompany(d: any): boolean {
   // const isValid = 'Title' in d && 'Subtitle' in d && 'Socials' in d && Array.isArray(d.Socials);
   const isValid = 'Title' in d && 'Subtitle' in d && 'Socials' in d;
-  if (!isValid) console.error('isValidICompany: novalid:', d);
+  if (!isValid) console.error('isValidICompany: no-valid:', d);
 
   return isValid;
 }
